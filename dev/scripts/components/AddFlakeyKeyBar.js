d0a62f48f073876c8af330c3438fa2553a86171e
Fatin Haque <frhaque@gmail.com>
2017-09-12 23:19:12 -0400

    Added add flakey bar

--- dev/scripts/components/AddFlakeyKeyBar.js
+++ dev/scripts/components/AddFlakeyKeyBar.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
-const AddFlakeyKeyBar = function(props) {
-    const {classEnvelope} = props.classes || {}; 
-    return (
-        <div className={classEnvelope}>AddFlakeyKeyBar needs work.</div>
-    );
+class AddFlakeyKeyBar extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            flakeyId: '',
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        e.preventDefault();
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({ [name]: value });
+
+        console.log(name);
+
+    }
+
+    render() {
+
+        return (
+            <form className="AddFlakeyBar" onSubmit={(e) => this.props.handleSubmit(e, this.state.flakeyId)}>
+                <input type="text" name="flakeyId" value={this.state.flakeyId} onChange={this.handleChange} placeholder="Enter shared Flakey key." />
+                <input type="submit" value="View Flakey" />
+            </form>
+        );
+    }
 }
 
 export default AddFlakeyKeyBar;
--- dev/scripts/components/Header.js
+++ dev/scripts/components/Header.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import LoggedInUserIndicator from './LoggedInUserIndicator.js';
+import LoggedInUserIndicator from './LoggedInUserIndicator';
+import AddFlakeyKeyBar from './AddFlakeyKeyBar';
 
-const Header = function(props) {
-    // const {classEnvelope, classTitle} = props.classes;
-    const {user, title, editFlakeyMode} = props;
-    const userName = user.name;
+class Header extends React.Component {
+    constructor() {
+        super();
 
-    const {handleCreateNewFlakey, handleToFlakeysView} = props;
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e, flakeyId) {
+        e.preventDefault();
+        this.props.history.push(`/flakeys/${flakeyId}`);
+
+    }
+
+    render() {
+        // const {classEnvelope, classTitle} = props.classes;
+        const {user, title, editFlakeyMode} = this.props;
+        const userName = user.name;
+
+        const {handleCreateNewFlakey, handleToFlakeysView} = this.props;
 
 
-    return (
-        <header className="Header">
-            <h1 className="Header__title">{title}</h1>
-            <button onClick={(e) => handleCreateNewFlakey(e, props.history)} >Create New Flakey</button>
-            {/* editFlakeyMode ? 
-                <button className="Header__btn Header__editBtn" onClick={handleEditFlakey}>Cancel</button>
-            : 
-                <button className="Header_btn Header__editBtn" onClick={handleEditFlakey}>Edit Flakey</button>
-            */}
-            
-            <button onClick={(e) => handleToFlakeysView(e, props.history)}>See My Flakeys</button>
+        return (
+            <header className="Header">
+                <h1 className="Header__title">{title}</h1>
+                <AddFlakeyKeyBar handleSubmit={this.handleSubmit} />
+                <button onClick={(e) => handleCreateNewFlakey(e, this.props.history)} >Create New Flakey</button>
+                {/* editFlakeyMode ? 
+                    <button className="Header__btn Header__editBtn" onClick={handleEditFlakey}>Cancel</button>
+                : 
+                    <button className="Header_btn Header__editBtn" onClick={handleEditFlakey}>Edit Flakey</button>
+                */}
+                
+                <button onClick={(e) => handleToFlakeysView(e, this.props.history)}>See My Flakeys</button>
 
-            <LoggedInUserIndicator userName={userName} className="Header__loggedInUserIndicator" />
-        </header>
-    );
+                <LoggedInUserIndicator userName={userName} className="Header__loggedInUserIndicator" />
+            </header>
+        );
+
+    }
 }
 
 export default Header;
